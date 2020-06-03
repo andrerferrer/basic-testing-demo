5f7e137e49498856bb4bb1a43fbc369aff010089
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-03 16:09:29 -0300

    add new create and show to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :products, only: %i[new create show]
   root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
