a49a2bef0b716649fc70d397a838c22bc700e4d3
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-03 16:00:55 -0300

    Write the tests for products new

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -7,4 +7,28 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Products"
     assert_selector ".card-product", count: Product.count
   end
+
+  test 'a logged in user can create a product' do
+    # Tell the test to: 
+    
+    # log in
+    login_as users(:player_one)
+
+    # visit the page to create a new product
+    visit '/products/new'
+
+    # fill in the form
+    fill_in "product_name",	with: "A wonderful product" 
+    fill_in "product_tagline",	with: "A wonderful tagline" 
+
+    # click on the create button
+    click_on 'Create Product'
+
+    # test if the test was taken to the product show page
+    assert_equal product_path(Product.last), page.current_path
+
+    # test if the product name shows in this page
+    assert_text "A wonderful product"
+
+  end
 end
