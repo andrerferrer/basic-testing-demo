b9cc64ad757394d7451ce8ec49c7829960f7b579
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-03 16:09:04 -0300

    Implement new show and create on controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,23 @@ class ProductsController < ApplicationController
   def index
   	@products = Product.all
   end
+
+  def show
+    @product = Product.find params[:id]
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new strong_params
+    @product.save ? redirect_to(@product) : render(:new)
+  end
+
+  private
+
+  def strong_params
+    params.require(:product).permit(:name, :tagline)
+  end
 end
