4a6d09b8f055d906f7d8357fd6fc679a0523e4f7
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-03 17:55:31 -0300

    Write tests for the user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fails saving if the user does not have an email" do
+    user = User.new password: '123456'
+    assert_equal user.valid?, false
+  end
+  
+  test "fails saving if the user does not have a password" do
+    user = User.new email: 'jack@sparrow.com'
+    assert_equal user.valid?, false
+  end
+  
+  test "saves if the user has an email and a valid password" do
+    user = User.new email: 'jack@sparrow.com', password: '123456'
+    assert_equal user.valid?, true
+  end
+
+  test "fails saving if the user has an email and an invalid password" do
+    user = User.new email: 'jack@sparrow.com', password: ''
+    assert_equal user.valid?, false
+  end
+
+  test "#full_name returns the correct name" do
+    user = User.new(
+      email: 'jack@sparrow.com',
+      password: '123456',
+      first_name: 'jack',
+      last_name: 'sparrow'
+    )
+
+    assert user.full_name, 'Jack Sparrow'
+
+  end
+
 end
