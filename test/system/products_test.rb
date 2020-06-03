46f2d697c04d921f7e99a96d6998309fb81b5ba1
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-03 14:59:37 -0300

    Generate products test

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit products_url
-  #
-  #   assert_selector "h1", text: "Product"
-  # end
+  test "visiting the index" do
+    visit root_path
+  
+     assert_selector "h1", text: "Products"
+  end
 end
